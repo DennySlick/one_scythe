a4d0cafe5514598bd2840487b1cf7e0451725929
DennySlick <ugoimba@mail.ru>
2018-09-08 19:37:56 +0300

    AddGame calculator pts for players in game. Bug with double save needed STILL(!). Added topmenu.

--- calculator/admin.py
+++ calculator/admin.py
@@ -9,6 +9,35 @@ class PlayerInGameInline(admin.TabularInline):
 
 class GameAdmin(admin.ModelAdmin):
     inlines = (PlayerInGameInline,)
+    exclude = ['cost', 'avg_sentinel', 'avg_scourge', 'count_players_sentinel', 'count_players_scourge']
+
+    #def save_model(self, request, obj, form, change):
+    #    if obj.playeringame_set:
+    #        #form.cleaned_data['players'] = obj.copy_holidays_from.players.all()
+    #        print("PLAYERINGAME_SET")
+    #        print(obj.playeringame_set.all())
+    #        if obj.avg_sentinel == 0 and obj.avg_scourge == 0:
+    #            for game_info in obj.playeringame_set.all():
+    #                if game_info.side == 'Sentinel':
+    #                    obj.avg_sentinel += game_info.pts_start
+    #                    obj.count_players_sentinel += 1
+    ##                    obj.avg_scourge += game_info.pts_start
+    #                    obj.count_players_scourge += 1
+    #            if obj.count_players_sentinel != 0 and obj.count_players_scourge != 0:
+    #                obj.avg_sentinel /= obj.count_players_sentinel
+    #                obj.avg_scourge /= obj.count_players_scourge
+#
+#                    if obj.cost == 0 and obj.winner != 'Draw':
+#                        if obj.winner == 'Sentinel':
+#                            obj.cost = int(obj.COST_CONST * obj.avg_scourge / obj.avg_sentinel)
+#                        elif obj.winner == 'Scourge':
+#                            obj.cost = int(obj.COST_CONST * obj.avg_sentinel / obj.avg_scourge)
+#                        for game_info in obj.playeringame_set.all():
+#                            game_info.save()
+#        else:
+#            print("NO SET")
+#
+#        super(GameAdmin, self).save_model(request, obj, form, change)
 
 class PlayerAdmin(admin.ModelAdmin):
     inlines = (PlayerInGameInline,)
--- calculator/models.py
+++ calculator/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.db.models.signals import m2m_changed, post_save
+from django.dispatch import receiver
 
+import datetime
+
+DEBUG_OUTPUT = True
+def debug_output(line):
+    if DEBUG_OUTPUT:
+        date = datetime.datetime.now()
+        date = date.strftime('[%d/%b/%Y %H:%M:%S]')
+        print(str(date) + " DEBUG_OUTPUT: \"" + line + "\"")
 
 class Player(models.Model):
     nickname = models.CharField(max_length=25)
@@ -10,12 +20,11 @@ class Player(models.Model):
         ordering = ['-pts', 'nickname']
 
     def __str__(self):
-        return " : ".join([self.nickname, str(self.pts)])
+        return " | ".join([self.nickname, str(self.pts)])
 
 
 class GameMode(models.Model):
     line = models.CharField(max_length=15)
-
     def __str__(self):
         return self.line
 
@@ -46,21 +55,53 @@ class Game(models.Model):
         choices=WINNER_CHOICES,
         default=DRAW,
     )
-    cost = models.IntegerField(default=25)
+    cost = models.IntegerField(default=0)
+    COST_CONST = 25
+
+    avg_sentinel = models.IntegerField(default=0)
+    avg_scourge = models.IntegerField(default=0)
+    count_players_sentinel = models.IntegerField(default=0)
+    count_players_scourge = models.IntegerField(default=0)
 
     class Meta:
         ordering = ['-date']
 
-    def __str__(self):
-        return (str(self.mode) + " #" + str(self.id))
+    def get_game_cost(self):
+        if self.avg_sentinel == 0 and self.avg_scourge == 0 and self.playeringame_set:
+            for game_info in self.playeringame_set.all():
+                if game_info.side == 'Sentinel':
+                    self.avg_sentinel += game_info.pts_start
+                    self.count_players_sentinel += 1
+                elif game_info.side == 'Scourge':
+                    self.avg_scourge += game_info.pts_start
+                    self.count_players_scourge += 1
+            if self.count_players_sentinel != 0 and self.count_players_scourge != 0:
+                self.avg_sentinel /= self.count_players_sentinel
+                self.avg_scourge /= self.count_players_scourge
 
+                if self.cost == 0 and self.winner != 'Draw':
+                    if self.winner == 'Sentinel':
+                        self.cost = int(self.COST_CONST * self.avg_scourge / self.avg_sentinel)
+                    elif self.winner == 'Scourge':
+                        self.cost = int(self.COST_CONST * self.avg_sentinel / self.avg_scourge)
+                    for game_info in self.playeringame_set.all():
+                        game_info.save()
+        else:
+            pass
+
+    def save(self, *args, **kwargs):
+        self.get_game_cost()
+        return super(Game, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return (str(self.mode) + " #" + str(self.id)) + " | Avg: " + str(int( (self.avg_sentinel + self.avg_scourge)/2))
 
 class PlayerInGame(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='playeringame_set')
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='playeringame_set')
 
     pts_start = models.IntegerField(default=None)
-    pts_dif = models.IntegerField(default=None)
+    pts_dif = models.IntegerField(default=0)
 
     SENTINEL = 'Sentinel'
     SCOURGE = 'Scourge'
@@ -74,24 +115,59 @@ class PlayerInGame(models.Model):
         default=SENTINEL,
     )
 
+
     class Meta:
         ordering = ['game', '-side', 'player']
 
-    def get_points(thePlayer):
-        return thePlayer.pts
-
-    def save(self):
+    def get_pts_start(self):
         if self.player != None and self.pts_start == None:
             self.pts_start = self.player.pts
-        if self.game != None and self.pts_dif == None:
-            self.pts_dif = self.game.cost
-            if self.game.winner == self.side:
-                self.player.pts += self.pts_dif
-            elif self.game.winner != 'Draw':
-                self.player.pts -= self.pts_dif
-            self.player.save()
+        else:
+            pass
 
+    def change_player_pts(self):
+        if (int(self.pts_dif) >= 0):
+            debug_output("CHANGE PLAYER PTS: " + str(self.player) + " +" + str(self.pts_dif))
+        else:
+            debug_output("CHANGE PLAYER PTS: " + str(self.player) + " " + str(self.pts_dif))
+        self.player.pts += self.pts_dif
+        self.player.save()
+
+    def get_pts_changes(self):
+        debug_output("GET PTS CHANGES: " + str(self))
+        if self.game != None and self.pts_dif == 0:
+            debug_output("TRY PTS CHANGES: " + str(self))
+            if self.game.cost != 0 and self.game.winner != 'Draw':
+                if self.game.winner == self.side:
+                    self.pts_dif = self.game.cost
+                #elif self.game.winner == 'Draw':
+                #    self.pts_dif = 0
+                else:
+                    self.pts_dif = -(self.game.cost)
+                self.change_player_pts()
+            else:
+                debug_output("GAME COST = 0: " + str(self))
+        else:
+            debug_output("CAN'T CHANGE PTS")
+            pass
+
+    def save(self):
+        self.get_pts_start()
+        self.get_pts_changes()
         super(PlayerInGame, self).save()
 
+
     def __str__(self):
-        return (str(self.game) + " | " + str(self.player)) + " | " + self.side
+        return (str(self.game) + " ][ " + str(self.player)) + " | " + self.side
+
+
+@receiver(post_save, sender=Game)
+def game_changed(sender, **kwargs):
+    instance = kwargs.pop('instance', None)
+    debug_output("GAME CHANGED: " + str(instance))
+
+
+@receiver(post_save, sender=PlayerInGame)
+def playeringame_changed(sender, **kwargs):
+    instance = kwargs.pop('instance', None)
+    debug_output("PlayerInGame CHANGED: " + str(instance))
